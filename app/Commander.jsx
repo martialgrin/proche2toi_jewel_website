6f29279ad6d97b9cb4dc496de52f80eb392b62ce
Martial Grin <49276099+martialgrin@users.noreply.github.com>
2025-09-21 13:13:36 +0200

    add use effect to resolve bug

--- app/Commander.jsx
+++ app/Commander.jsx
@@ -1,20 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 export const Commander = ({obj, callback}) => {
 
-
 const [sold, setSold] = useState(obj.sold);
-const className = sold ? "commander-container sold" : "commander-container";
 const [content, setContent] = useState("Commander");
-    const handleCommander = (e) => {
-        if(content === "Commander") {
-            setContent("Commander");
-        } else {
-            setContent("Commander");
-        }
-        console.log(obj);
-        callback(obj);
+let className = sold ? "commander-container sold" : "commander-container";
 
+
+// Update sold state when obj.sold changes
+useEffect(() => {
+    setSold(obj.sold);
+    className = sold ? "commander-container sold" : "commander-container";
+}, [obj.sold]);
+
+
+const handleCommander = (e) => {
+    if(content === "Commander") {
+        setContent("Commandé");
+    } else {
+        setContent("Commander");
     }
+    console.log(obj);
+    callback(obj);
+}
 	return (
         <div className={className + " pill-button"} onClick={(e) => handleCommander(e)} disabled={obj.sold}>
         {obj.sold && <p>Vendu</p>}
